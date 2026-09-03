a0c480730ec4524af604e920c558aed0b93da5d7
SHIRongli2627 <keatser070104@gmail.com>
2026-09-03 09:04:08 +0000

    [skip actions] lab/lab01/ch01_t04_handling_errors.py

--- lab/lab01/ch01_t04_handling_errors.py
+++ lab/lab01/ch01_t04_handling_errors.py
@@ -1,2 +1,2 @@
 print("Mismatched quotes will cause a SyntaxError")# print("How do you make a hot dog stand?')
-# print(You take away its chair!)
+print(You take away its chair!)# print(You take away its chair!)
