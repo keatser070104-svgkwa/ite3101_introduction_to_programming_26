d7fd5cca4d244e76d70b80a532ac2ae9a4cc87b8
SHIRongli2627 <keatser070104@gmail.com>
2026-09-17 08:56:17 +0000

    [skip actions] lab/lab02/ch02_t14_string_formatting_with_percentage_1.py

--- lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
+++ lab/lab02/ch02_t14_string_formatting_with_percentage_1.py
@@ -1,5 +1,5 @@
 string_1 = "Camelot"
 string_2 = "place"
 
-#print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
-print("Let's not go to string_1{string_1}. 'Tis a silly %s." % (string_1, string_2))
+# print("Let's not go to %s. 'Tis a silly %s." % (string_1, string_2))
+print(f"Let's not go to string_1{string_1}. 'Tis a silly {string_2}.")
