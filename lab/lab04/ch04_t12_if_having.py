02965e973cb92b17d0f2b2f5de5dd5da5e02c523
SHIRongli2627 <keatser070104@gmail.com>
2026-09-24 08:40:50 +0000

    lab/lab04/ch04_t12_if_having.py

--- lab/lab04/ch04_t12_if_having.py
+++ lab/lab04/ch04_t12_if_having.py
@@ -1,10 +1,10 @@
 def using_control_once() -> str:
-    if True:
+    if 2 > 1:
         return "Success #1"
 
 
 def using_control_again() -> str:
-    if None:
+    if 2 == 2:
         return "Success #2"
 
 
